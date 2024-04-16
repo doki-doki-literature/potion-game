6c6d3e839d4dab466e9d447901346aaf248db1fa
Colin <colinmkim@gmail.com>
2024-04-16 17:22:56 -0400

    add book background for archive

--- src/scenes/ArchiveBook.ts
+++ src/scenes/ArchiveBook.ts
@@ -11,9 +11,15 @@ export class ArchiveScene extends Phaser.Scene {
         super(sceneConfig);
     }
 
+    preload() {
+        this.load.image("bookBackground", "assets/image/book_background.png");
+    }
+
     create() {
         // Create a back button
         const backButton = this.add.text(20, 50, "Back to Cabin").setInteractive();
         backButton.on("pointerdown", () => this.scene.start("Cabin"));
+
+        this.add.image(400, 300, "bookBackground").setScale(.08, .08).setDepth(-1);
     }
 }
