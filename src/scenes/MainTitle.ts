a67348822703c9e3b585ecabb1f02a4c96fe5b76
qcluu <lcao29@gmail.com>
2024-04-20 06:51:37 -0400

    added some placeholder background music

--- src/scenes/MainTitle.ts
+++ src/scenes/MainTitle.ts
@@ -10,8 +10,14 @@ export class MainTitleScene extends Phaser.Scene {
     constructor() {
         super(sceneConfig);
     }
+    preload() {
+        this.load.audio('backgroundMusic', 'assets/sound/backgroundMusic.mp3')
+    }
 
     create() {
+        const backgroundMusic = this.sound.add('backgroundMusic', { loop: true, volume: 0.05 });
+        backgroundMusic.play();
+
         this.add.text(450, 300, "Potion Gaem");
         const startButton = this.add.text(450, 350, "Start").setInteractive();
         startButton.on("pointerdown", () => this.scene.start("Cabin"));
